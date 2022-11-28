dab73db232e680719431b51f45f52b359bbeab1a
Steven Devonport <steven.devonport@fastmail.com>
2022-11-28 13:53:12 +0000

    Added world cell position to debug statement

--- Camera2D.gd
+++ Camera2D.gd
@@ -6,7 +6,16 @@ var zoomMax: float = 2.0
 var dragSensitivity: float = 1.0
 var zoom_level :=1.0 setget _set_zoom_level
 
+onready var cellLabel = get_parent().get_node("debug/HBoxContainer/cell/cellLabel")
+
+
 func _input(event):
+
+	if event is InputEventMouseMotion:
+		var mousePosition = get_parent().get_node("TileMap").world_to_map(get_global_mouse_position())
+#		World Cell (X/Y): 00/00
+		cellLabel.text = "World Cell (X/Y): " + str(mousePosition)
+
 	# drag the map around
 	if event is InputEventMouseMotion and Input.is_mouse_button_pressed(BUTTON_RIGHT):
 		position -= event.relative * dragSensitivity / zoom
